dd36127a5641a52b1735ac07f5dc9083296b4fad
Dale Jung <dale@dalejung.com>
2015-03-31 09:16:42 -0400

    Moved most logic to protocol.py

--- zipline/dataverse/backtest/bardata.py
+++ zipline/dataverse/backtest/bardata.py
@@ -1,7 +1,5 @@
 import zipline.protocol as zp
 
-from six import iteritems, iterkeys
-
 
 class BacktestSIDData(object):
     def __init__(self, sid, dataverse, initial_values=None):
@@ -66,74 +64,3 @@ class BacktestBarData(zp.BarData):
         self._data = data or {}
         self._contains_override = None
         self._siddata_class = siddata_class
-
-    _contains_override_ = None
-
-    @property
-    def _contains_override(self):
-        return self._contains_override_
-
-    @_contains_override.setter
-    def _contains_override(self, value):
-        self._contains_override_ = value
-        self._keys_cache_ = None
-
-    _keys_cache = None
-
-    @property
-    def _keys_cache(self):
-        if self._keys_cache_ is None:
-            keys = self._data.keys()
-            if self._contains_override:
-                keys = list(filter(self._contains_override, keys))
-            self._keys_cache_ = keys
-        return self._keys_cache_
-
-    def __contains__(self, name):
-        return name in self._keys_cache
-
-    def get_default(self, name):
-        try:
-            sid_data = self[name]
-        except KeyError:
-            sid_data = self[name] = self._siddata_class(name, self._dataverse)
-        return sid_data
-
-    def __setitem__(self, name, value):
-        self._data[name] = value
-        self._keys_cache_ = None
-
-    def __getitem__(self, name):
-        return self._data[name]
-
-    def __delitem__(self, name):
-        del self._data[name]
-        self._keys_cache_ = None
-
-    def __iter__(self):
-        keys_cache = self._keys_cache
-        for sid, data in iteritems(self._data):
-            # Allow contains override to filter out sids.
-            if sid in keys_cache:
-                if len(data):
-                    yield sid
-
-    def iterkeys(self):
-        # Allow contains override to filter out sids.
-        keys_cache = self._keys_cache
-        return (sid for sid in iterkeys(self._data) if sid in keys_cache)
-
-    def itervalues(self):
-        return (value for _sid, value in self.iteritems())
-
-    def iteritems(self):
-        keys_cache = self._keys_cache
-        return ((sid, value) for sid, value
-                in iteritems(self._data)
-                if sid in keys_cache)
-
-    def __len__(self):
-        return len(self.keys())
-
-    def __repr__(self):
-        return '{0}({1})'.format(self.__class__.__name__, self._data)
